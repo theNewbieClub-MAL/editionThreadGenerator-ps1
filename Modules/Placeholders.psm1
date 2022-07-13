6099d0e3793612bf456e83c7752695a396813bb3
Natsu Tadama <nattadasu@nattadasu.my.id>
2022-07-13 13:18:13 +0700

    Add Randomizer for Image Placeholder

---
+++ Modules/Placeholders.psm1
@@ -0,0 +1,101 @@
+Function Get-RandomBanner {    
+    if (Test-Path ./Sources/Banners) {
+        $randomBanner = (Get-ChildItem -Path ./Sources/Banners/ | Get-Random).Name
+        Write-Verbose "$($randomBanner) has been set"
+        $randomBanner
+    } else {
+        $WarningMessage = "./Sources/Banners can not be found on $(Get-Location)"
+    	Write-Host $WarningMessage -ForegroundColor Red
+    	Write-Verbose $WarningMessage
+    }
+
+    <#
+        .SYNOPSIS
+        Get Random Banner placeholder filename
+        
+        .DESCRIPTION
+        Get Random Banner placeholder filename that is available on ./Sources/Banners
+
+        .EXAMPLE
+        PS> Get-RandomBanner
+        un1corn_tnc@banner1_v1.png
+
+        .NOTES
+        Function will not work if ./Sources/Banners directory is not exist.
+
+        .LINK
+        Get-RandomCard
+
+        .LINK
+        Get-WorkStaff
+    #>
+
+}
+
+Function Get-RandomCard {
+    $randomCard = (Get-ChildItem -Path ./Sources/Cards/ | Get-Random).Name
+    Write-Verbose "$($randomCard) has been set"
+    $randomCard
+
+    <#
+        .SYNOPSIS
+        Get Random Card placeholder filename
+        
+        .DESCRIPTION
+        Get Random Card placeholder filename that is available on ./Sources/Cards
+
+        .EXAMPLE
+        PS> Get-RandomCard
+        un1corn_tnc@cards1_v1.png
+
+        .NOTES
+        Function will not work if ./Sources/Cards directory is not exist.
+
+        .LINK
+        Get-RandomBanner
+
+        .LINK
+        Get-WorkStaff
+    #>
+}
+
+# Function Get-WorkStaff {
+# 	param (
+# 		[Parameter(Mandatory=$true)]
+# 		[string]$FileName
+# 	)
+
+# 	$StaffQuery = $FileName -match '^[\w\-_.]{1,16}'
+# 	[regex]::Match($StaffQuery, "^[\w\-_]+").Groups[0].Value
+#     <#
+#         .SYNOPSIS
+#         Extract staff name from file with RegEx
+
+#         .DESCRIPTION
+#         Extract a staff name from file with RegEx.
+#         This function is optimized to use for MyAnimeList username rules:
+#         Alphanumeric case-sensitive with dash and underscore up to 16 characters ("[\w\-_]{,16}")
+
+#         .PARAMETER FileName
+#         Specifies the file name.
+
+#         .EXAMPLE
+#         PS> $FileName = Get-RandomCard
+
+#         PS> $FileName
+#         un1corn_tnc@cards1_v1.png
+
+#         PS> Get-WorkStaff -FileName $FileName
+#         un1corn_tnc
+
+#         .LINK
+#         Get-RandomBanner
+
+#         .LINK
+#         Get-RandomCard
+#     #>
+# }
+
+Export-ModuleMember -Function Get-RandomBanner
+Export-ModuleMember -Function Get-RandomCard
+# Export-ModuleMember -Function Get-WorkStaff
